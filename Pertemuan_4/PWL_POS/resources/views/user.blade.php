1837725f5c50a07aa8026c3f4de74ec396ab2f40
Raudhil <raudhilfirdausn@gmail.com>
2025-03-06 21:47:41 +0700

    PWL Praktikum 2.7 Relationship

--- Pertemuan_4/PWL_POS/resources/views/user.blade.php
+++ Pertemuan_4/PWL_POS/resources/views/user.blade.php
@@ -14,6 +14,9 @@
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
+            <td>Kode Level</td>
+            <td>Nama Level</td>
+            <td>Aksi</td>
         </tr>
         @foreach ($data as $d)
         <tr>
@@ -21,6 +24,8 @@
             <td>{{ $d->username}}</td>
             <td>{{ $d->nama}}</td>
             <td>{{ $d->level_id}}</td>
+            <td> {{ $d->level->level_kode}}</td>
+            <td>{{ $d->level->level_nama}}</td>
             <td><a href="/PWL/Pertemuan_4/PWL_POS/public/user/ubah/{{ $d->user_id }}">ubah</a> | <a href="/PWL/Pertemuan_4/PWL_POS/public/user/hapus/{{ $d->user_id }}">hapus</a></td>
         </tr>
         @endforeach
