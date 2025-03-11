3accd1e05122e0980074998d64888011964a1a36
Raudhil <raudhilfirdausn@gmail.com>
2025-03-11 21:21:42 +0700

    PWL Praktikum 4 Implementasi Filtering Datatables

--- Pertemuan_4/PWL_POS/app/Http/Controllers/UserController.php
+++ Pertemuan_4/PWL_POS/app/Http/Controllers/UserController.php
@@ -24,7 +24,9 @@ class UserController extends Controller
 
         $activeMenu = 'user';
 
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
+        $level = LevelModel::all();
+
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'level' => $level,'activeMenu' => $activeMenu]);
     }
 
     // Ambil data user dalam bentuk json untuk datatables
@@ -32,6 +34,10 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+            if($request->level_id){
+                $users->where('level_id', $request->level_id);
+            }
 
         return DataTables::of($users)
             // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
