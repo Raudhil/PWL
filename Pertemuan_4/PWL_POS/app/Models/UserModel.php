dc0fc292e3916fdd98394b20adbd58797956a24b
Raudhil <raudhilfirdausn@gmail.com>
2025-05-03 20:27:48 +0700

    Jobsheet 11 Praktikum 1

--- Pertemuan_4/PWL_POS/app/Models/UserModel.php
+++ Pertemuan_4/PWL_POS/app/Models/UserModel.php
@@ -8,14 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
-
-    protected $table = 'm_user';
-    protected $primaryKey = 'user_id';
-
     public function getJWTIdentifier()
     {
         return $this->getKey();
@@ -26,38 +22,26 @@ class UserModel extends Authenticatable implements JWTSubject
         return [];
     }
 
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+
     protected $fillable = [
-        'level_id',
         'username',
         'nama',
-        'password'
+        'password',
+        'level_id',
+        'image' // tambahan
     ];
 
-    protected $hidden = [
-        'password'
-    ];
-
-    protected $casts = [
-        'password' => 'hashed'
-    ];
-
-    public function level(): BelongsTo
+    public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
-    public function getRoleName(): string
+    protected function image(): Attribute
     {
-        return $this->level->level_kode;
-    }
-
-    public function hasRole($role): bool
-    {
-        return $this->level->level_kode === $role;
-    }
-
-    public function getRole()
-    {
-        return $this->level->level_kode;
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
     }
 }
